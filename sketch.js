3a4cdfd8df1f49b19d705891dab58dab60c78383
antonysiby <91169646+antonysiby@users.noreply.github.com>
2021-10-15 16:36:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,71 +1,75 @@
-  var garden,rabbit;
-  var gardenImg,rabbitImg;
-  var apple,appleImg 
-  var leaf,leafImg
+var garden,rabbit,apple,orangeL,redL;
+var gardenImg,rabbitImg,carrotImg,orangeImg,redImg;
 
 
 function preload(){
   gardenImg = loadImage("garden.png");
-  rabbitImg = loadImage("rabbit.png");  
+  rabbitImg = loadImage("rabbit.png");
   appleImg = loadImage("apple.png");
-  leafImg = loadImage("leaf.png")
- }
-
-function setup(){ 
-  createCanvas(400,400);
-
-  garden = createSprite(200,200);
-  garden.addImage(gardenImg);
-
-  rabbit = createSprite(180,340,30,30);
-  rabbit.scale =0.09;
-  rabbit.addImage(rabbitImg);
-
+  orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png");
 }
 
+
+function setup(){
+  
+  createCanvas(400,400);
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+
+//creating boy running
+rabbit = createSprite(160,340,20,20);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
 
 function draw() {
   background(0);
   
-  rabbit.x = World.mouseX
+  // boy moving on Xaxis with mouse'
+  rabbit.x = World.mouseX;
   
-
-  createLeaves();
-  createApples();
-
-  var select_apples = Math.round(random(50, 12500));
-  var select_leaves = Math.round(random(5,12500));
+  edges = createEdgeSprites();
+  rabbit.collide(edges);
   
-
-
-  if (World.frameCount % 80 == 0) {
-    if(select_apples == 1){
-      createApples()
+   drawSprites();
+  
+  var select_sprites = Math.round(random(1,5));
+  
+  if (frameCount % 100 == 0) {
+    if (select_sprites == 3) {
+      createApples();
+    } else if (select_sprites == 1) {
+      createOrange();
+    }else {
+      createRed();
     }
-    else{
-      createLeaves()
-    }
-  }    
-
-
-  drawSprites()
   }
 
-function createLeaves(){
-  leaf = createSprite(random(5,12500),40,10,10);
-  leaf.addImage(leafImg);
-  leaf.scale = 0.05
-  leaf.velocityY = 3
-  leaf.lifetime = 900
 }
 
-function createApples(){
-  apple = createSprite(random(50,12500),40,10,10);
-  apple.addImage(appleImg);
-  apple.scale = 0.05
-  apple.velocityY = 3
-  apple.lifetime = 900
+function createApples() {
+apple = createSprite(random(100, 350),40, 10, 10);
+apple.addImage(appleImg);
+apple.scale=0.05;
+apple.velocityY = 3;
+apple.lifetime = 900;
 }
 
+function createOrange() {
+orangeL = createSprite(random(100, 350),40, 10, 10);
+orangeL.addImage(orangeImg);
+orangeL.scale=0.08;
+orangeL.velocityY = 3;
+orangeL.lifetime = 900;
+}
 
-
+function createRed() {
+redL = createSprite(random(100, 350),40, 10, 10);
+redL.addImage(redImg);
+redL.scale=0.06;
+  redL.velocityY = 3;
+  redL.lifetime = 900;
+}
